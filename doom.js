b2a13b8c4ad89d42c4bf0a1efebd5e4f762c5c82
Ricardo Bittencourt <bluepenguin@gmail.com>
2016-01-09 23:56:25 -0200

    Refactor Stage

--- doom.js
+++ doom.js
@@ -18,6 +18,34 @@
     return (str + '\0').split('\0').shift();
   }
 
+  function Scaler(limits) {
+    this.limits = limits;
+  }
+
+  function Stage() {
+    this.vertexes = {x: [], y: []};
+    this.lines = [];
+    this.scaler = 0;
+  }
+
+  Stage.prototype.push_vertex = function(vertex) {
+    this.vertexes.x.push(vertex.x);
+    this.vertexes.y.push(vertex.y);
+  };
+
+  Stage.prototype.push_line = function(line) {
+    this.lines.push(line);
+  };
+
+  Stage.prototype.optimize = function() {
+    this.scaler = new Scaler({
+      minx: _.min(this.vertexes.x),
+      maxx: _.max(this.vertexes.x),
+      miny: _.min(this.vertexes.y),
+      maxy: _.max(this.vertexes.y),
+    });
+  };
+
   function Wad(wad) {
     this.wad = wad;
     this.directory = [];
@@ -42,62 +70,55 @@
   };
 
   Wad.prototype.parse_stage = function(stage_name) {
-    var stage = {};
+    var stage = new Stage();
     var start = _.findWhere(this.directory, {name: stage_name}).index;
-    stage.vertexes = this.parse_vertexes(start);
-    stage.lines = this.parse_lines(start);
+    var assets = _.rest(this.directory, start);
+    this.parse_vertexes(assets, stage);
+    this.parse_lines(assets, stage);
     return stage;
   };
 
-  Wad.prototype.parse_vertexes = function(start) {
-    var assets = _.rest(this.directory, start);
+  Wad.prototype.parse_vertexes = function(assets, stage) {
     var entry = _.findWhere(assets, {name: "VERTEXES"});
-    var vertexes = {x: [], y: []};
-    vertexes.size = entry.size / 4;
-    for (var i = 0; i < vertexes.size; i++) {
-      vertexes.x.push(this.wad.getInt16(entry.start + i * 4, true));
-      vertexes.y.push(this.wad.getInt16(entry.start + i * 4 + 2, true));
+    var size = entry.size / 4;
+    for (var i = 0; i < size; i++) {
+      stage.push_vertex({
+        x: this.wad.getInt16(entry.start + i * 4, true),
+        y: this.wad.getInt16(entry.start + i * 4 + 2, true)
+      });
     }
-    vertexes.minx = _.min(vertexes.x);
-    vertexes.maxx = _.max(vertexes.x);
-    vertexes.miny = _.min(vertexes.y);
-    vertexes.maxy = _.max(vertexes.y);
-    return vertexes;
   };
 
-  Wad.prototype.parse_lines = function(start) {
-    var assets = _.rest(state.wad.directory, start);
+  Wad.prototype.parse_lines = function(assets, stage) {
     var entry = _.findWhere(assets, {name: "LINEDEFS"});
-    var lines = [];
-    lines.size = entry.size / 14;
-    for (var i = 0; i < lines.size; i++) {
-      lines.push({
+    var size = entry.size / 14;
+    for (var i = 0; i < size; i++) {
+      stage.push_line({
         begin: this.wad.getUint16(entry.start + i * 14 + 0, true),
         end: this.wad.getUint16(entry.start + i * 14 + 2, true)
       });
     }
-    return lines;
   };
 
   function scale(x, minx, maxx, size) {
     return (x - minx) / (maxx - minx) * size;
   }
 
-  function draw_stage() {
+  function draw_stage(stage) {
     var ctx = $("#playfield")[0].getContext("2d");
     ctx.clearRect(0, 0, 500, 500);
-    var vertexes = state.stage.vertexes;
-    var lines = state.stage.lines;
-    for (var i = 0; i < lines.size; i++) {
-      ctx.beginPath();
+    var vertexes = stage.vertexes;
+    var lines = stage.lines;
+    for (var i = 0; i < lines.length; i++) {
+      ctx.beginPath();      
       ctx.moveTo(scale(vertexes.x[lines[i].begin], 
-                       vertexes.minx, vertexes.maxx, 500),
+                       stage.scaler.limits.minx, stage.scaler.limits.maxx, 500),
                  scale(vertexes.y[lines[i].begin], 
-                       vertexes.miny, vertexes.maxy, 500));
+                       stage.scaler.limits.miny, stage.scaler.limits.maxy, 500));
       ctx.lineTo(scale(vertexes.x[lines[i].end], 
-                       vertexes.minx, vertexes.maxx, 500),
+                       stage.scaler.limits.minx, stage.scaler.limits.maxx, 500),
                  scale(vertexes.y[lines[i].end], 
-                       vertexes.miny, vertexes.maxy, 500));
+                       stage.scaler.limits.miny, stage.scaler.limits.maxy, 500));
       ctx.stroke();
     }
   }
@@ -117,18 +138,15 @@
     });
     select.change(function() {
       var name = $(this).find(":selected").text();
-      enable_stage(name);
+      var stage = state.wad.parse_stage(name);
+      stage.optimize();
+      draw_stage(stage);
     });
   }
 
   function load_first_stage() {
     $("#stage_select option:first").attr('selected', 'selected');
     $("#stage_select").trigger("change");
-  }
-
-  function enable_stage(name) {
-    state.stage = state.wad.parse_stage(name);
-    draw_stage();
   }
 
   function load_wad() {
